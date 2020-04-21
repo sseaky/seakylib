2be82b599385bc74c3bb0a53dcacbc7b63d4d6fb
seaky <seaky.cn@gmail.com>
2020-04-21 11:35:00 +0800

    soft_options/ssh_quite

--- seakylib/os/cmd.py
+++ seakylib/os/cmd.py
@@ -40,27 +40,35 @@ def execute(cmd, stdout=PIPE, stderr=STDOUT, encoding='utf-8', shell=False, *arg
     return p
 
 
-def make_ssh_cmd(cmd, ssh_host, ssh_port=22, ssh_user=None, ssh_keyfile=None, env=None, escape='`"', **kwargs):
+def make_ssh_cmd(cmd, ssh_host, ssh_port=22, ssh_user=None, ssh_keyfile=None, env=None, escape='`"', ssh_options=None,
+                 ssh_quite=False, **kwargs):
     '''
     :param ssh_host:
     :param cmd:
     :param ssh_port:
     :param ssh_user:
     :param ssh_keyfile:
-    :param ssh_pkey:
     :param env:
     :param escape:  替换 ` "
+    :param ssh_options:  {'StrictHostKeyChecking': 'no', 'UserKnownHostsFile': '/dev/null'}
+    :param ssh_quite:  ssh -q, 对于执行sql，warning信息会干扰结果的自动解析
     :param kwargs:
     :return:
     '''
     if isinstance(escape, str):
         for es in list(escape):
             cmd = re.sub(r'(?<!\\){}'.format(es), '\\{}'.format(es), cmd)
-    cmd1 = "ssh -p {} ".format(ssh_port)
+    cmd1 = 'ssh '
+    if ssh_quite:
+        cmd1 += '-q '
+    cmd1 += "-p {} ".format(ssh_port)
     if ssh_user:
         cmd1 += "-l {} ".format(ssh_user)
     if ssh_keyfile:
         cmd1 += "-i '{}' ".format(ssh_keyfile)
+    if isinstance(ssh_options, dict):
+        for k, v in ssh_options.items():
+            cmd1 += '-o {}={} '.format(k, v)
     cmd1 += ssh_host
     if isinstance(env, dict):
         cmd = ''.join("export {}='{}' && ".format(k, v) for k, v in env.items()) + cmd
@@ -131,6 +139,7 @@ def execute_sql_remote(sql, **kwargs):
         sql
     :return:
     '''
+    kwargs['ssh_quite'] = True
     p = execute_ssh_cmd(cmd=make_cmd_sql(sql, **kwargs), **kwargs)
     return parse_execute_sql_result(p)
 
